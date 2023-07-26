1fc2b32845257c6eb419b4ecbb43c9c609061254
Aly Mammoun <53837104+alymaamoun@users.noreply.github.com>
2023-07-26 10:36:41 +0300

    Add files via upload

---
+++ Unit 3 Embedded C/Lesson 2/source/uart.c
@@ -0,0 +1,17 @@
+#include "uart.h"
+
+
+#define New_Reg_M(Address,Offset,size) *((volatile size * const)((size*)(Address+Offset)))
+
+#define UART0DR_T New_Reg_M(0x101f1000,0,unsigned int)
+
+int uninitilized_global_variable;
+
+void Uart_Send_String(unsigned char* P_Tx_String)
+{
+	while(*P_Tx_String!='\0')
+	{
+		UART0DR_T=(unsigned int)(*P_Tx_String);
+		P_Tx_String++;
+	}
+} 
